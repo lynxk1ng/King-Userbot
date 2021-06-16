83bca409bf3addc747a8b1da0542c8a85b12af0f
lynxk1ng <85977651+lynxk1ng@users.noreply.github.com>
2021-06-16 16:53:13 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -107,7 +107,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/apisuserbot/King-Userbot.git")
+    "https://github.com/lynxk1ng/King-Userbot.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "King-Userbot")
 
