09ad59d06246f71a2be59edf54677f7838ee5875
lynxk1ng <85977651+lynxk1ng@users.noreply.github.com>
2021-06-18 09:17:10 +0700

    Update kekuatan.py

--- userbot/modules/kekuatan.py
+++ userbot/modules/kekuatan.py
@@ -42,7 +42,7 @@ from userbot import bot, TEMP_DOWNLOAD_DIRECTORY, CMD_HELP
 
 @register(outgoing=True, pattern=r'^.kekuatan(:? |$)([1-8])?')
 async def _(fry):
-    await fry.edit("`King Mengaktifkan Kekuatan Telegram...⚡`")
+    await fry.edit("`Orang Ganteng Mengaktifkan Kekuatan Telegram...⚡`")
     level = fry.pattern_match.group(2)
     if fry.fwd_from:
         return
@@ -54,7 +54,7 @@ async def _(fry):
         await fry.edit("`Gambar tidak di dukung`")
         return
     if reply_message.sender.bot:
-        await fry.edit("`Mohon Balas Di Sticker King`")
+        await fry.edit("` Balas Ke Sticker Tolol`")
         return
     chat = "@image_deepfrybot"
     message_id_to_reply = fry.message.reply_to_msg_id
